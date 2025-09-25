236e62530c1186cebaaa8d6b5be9ef6d64c37ce0
Admin <Admin@192.168.100.225>
2025-09-25 11:18:03 +0700

    nộp lab2 Signed-off-by:Admin <Admin@192.168.100.225>

--- Lab2/src/main/java/com/mycompany/lab2/bai1.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,21 @@ package com.mycompany.lab2;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        System.out.println("nhap a: ");
+        a=sc.nextInt();
+        System.out.println("nhap b: ");
+        b=sc.nextInt();
+       if (a == 0) {
+            if (b == 0) {
+                System.out.println("Pt co vo so nghiem.");
+            } else {
+                System.out.println("Pt vo nghiem.");
+            }
+        } else {
+            double x = -b / a;
+            System.out.println("Nghiem cua pt la x = " + x);
+        }
     }
 }
--- Lab2/src/main/java/com/mycompany/lab2/bai3.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai3.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.lab2;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,18 @@ package com.mycompany.lab2;
 public class bai3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        double SoDien, TienDien;
+        System.out.println("nhap so dien su dung (kWh): ");
+        SoDien = sc.nextDouble();
+        if (SoDien < 0) {
+            System.out.println("so dien khong hop le");
+        }
+        if (SoDien < 50) {
+            TienDien = SoDien * 1000;
+        } else {
+            TienDien = SoDien * 50 + (SoDien - 50) * 1200;
+        }
+        System.out.print("tien dien phai tra: " + TienDien + "VND");
     }
 }
